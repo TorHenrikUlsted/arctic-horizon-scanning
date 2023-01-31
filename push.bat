1cfeeb88796b564947d18adeeb296a6f1d41d735
Tor Henrik Ulsted <tor_henrik_ulsted@hotmail.com>
2023-01-31 22:27:57 +0100

    made changes

---
+++ push.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\tor_h\OneDrive - NTNU\Master thesis\MasterCode\MasterThesis-NaturalizingPlants-Arctic"
+git add "C:\Users\tor_h\OneDrive - NTNU\Master thesis\MasterCode\MasterThesis-NaturalizingPlants-Arctic"
+git commit -m
+git push earthengine master
+git push GitHub dev
