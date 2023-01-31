a201851de21e251aae31b91a87350ea2dc9fdf8b
Tor Henrik Ulsted <tor_henrik_ulsted@hotmail.com>
2023-01-31 23:55:22 +0100

    test

---
+++ components/ensemble.js
@@ -0,0 +1,10 @@
+const bioVars_df = ee.Image("WORLDCLIM/V1/BIO");
+
+var annualMeanTemperature = dataset.select('bio01');
+var visParams = {
+  min: -230.0,
+  max: 300.0,
+  palette: ['blue', 'purple', 'cyan', 'green', 'yellow', 'red'],
+};
+Map.setCenter(71.72, 52.48, 3.0);
+Map.addLayer(annualMeanTemperature, visParams, 'Annual Mean Temperature');
