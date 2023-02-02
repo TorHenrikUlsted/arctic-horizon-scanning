aa54781465351aa5fd325a5d5be8eaf0fb4285fe
torhul <torhul@stud.ntnu.no>
2023-02-02 20:38:17 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -1,6 +1,6 @@
 var bioVars_df = ee.Image("WORLDCLIM/V1/BIO");
 var CircumpolarArctic = ee.Image("projects/master-thesis-375622/assets/CAVMmap");
-var cavm = ee.FeatureCollection(CircumpolarArctic).geometry();
+var cavm = ee.ImageCollection(CircumpolarArctic).geometry();
 
 Map.setCenter(-5.386895, 75.386895, 2.0);
 
