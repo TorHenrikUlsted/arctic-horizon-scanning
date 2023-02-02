225c91b274d8e4acc483ae9bf6366a6c85682866
torhul <torhul@stud.ntnu.no>
2023-02-02 20:04:09 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -12,7 +12,7 @@ var visParams = {
   palette: ['blue', 'purple', 'cyan', 'green', 'yellow', 'red'],
 };
 
-var aTemp = ee.ImageCollection([annualMeanTemp, visParams])
+var aTemp = ee.ImageCollection([annualMeanTemp, visParams)
 var warmestMonth = bioVars_df.select('bio05');
 var visParamsWarmestMonth = {
   min: -96,
