bb4c165e5bcee7202905eae45ee31367c64d884c
torhul <torhul@stud.ntnu.no>
2023-02-02 19:48:48 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -1,7 +1,7 @@
 var bioVars_df = ee.Image("WORLDCLIM/V1/BIO");
 var cavm = ee.Image("projects/master-thesis-375622/assets/CAVMmap");
 
-Map.addLayer(cavm, {bands: "b1"});
+Map.addLayer(cavm, {bands: "b2"});
 
 var annualMeanTemp = bioVars_df.select('bio01');
 var visParams = {
