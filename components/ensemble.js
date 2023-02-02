858490d39bbe543f66d385a01a9f9d1057cf0a84
torhul <torhul@stud.ntnu.no>
2023-02-02 19:47:12 +0000

    Updated components/ensemble.js (deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -17,7 +17,6 @@ var visParamsWarmestMonth = {
 };
 
 Map.setCenter(71.72, 52.48, 3.0);
-var coll1 = ee.ImageCollection([cavm, annualMeanTemp])
 //Map.addLayer(annualMeanTemp, visParams, 'Annual Mean Temperature');
 //Map.addLayer(warmestMonth, visParamsWarmestMonth, 'Warmest Month');
 
