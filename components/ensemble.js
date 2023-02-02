bbace381bc2b212c89e85bd53f2f233e4ed6b4f1
torhul <torhul@stud.ntnu.no>
2023-02-02 20:02:19 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -18,7 +18,7 @@ var visParamsWarmestMonth = {
   palette: ['blue', 'purple', 'cyan', 'green', 'yellow', 'red'],
 };
 
-Map.addLayer(cavm, visParams)
+Map.addLayer(cavm, annulaMeanTemp, visParams)
 //Map.addLayer(annualMeanTemp, visParams, 'Annual Mean Temperature');
 //Map.addLayer(warmestMonth, visParamsWarmestMonth, 'Warmest Month');
 
