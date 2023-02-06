0c8d060f2abe4838eb51802bb91e6249621e1575
torhul <torhul@stud.ntnu.no>
2023-02-06 15:10:39 +0000

    Updated components/ensemble.js (added 2 lines, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -4,7 +4,8 @@ var cavm = ee.FeatureCollection("projects/master-thesis-375622/assets/aga_circum
     glonaf = ee.FeatureCollection("projects/master-thesis-375622/assets/257_9_257_2_GloNAF_Shapefile"),
     cavmImg = ee.Image("projects/master-thesis-375622/assets/CAVMmap");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-var bioClip = bioVars.clip(cavm)
+var bioClip = bioVars.clip(cavm);
+var gloClip = cavmIMG.clip(glonaf);
 Map.centerObject(cavm);
 
 // Extract geometries from you regions 
