078db8cb7a9b64c06f2480a537d7db4fbe921e5d
torhul <torhul@stud.ntnu.no>
2023-02-06 20:37:25 +0000

    Updated components/ensemble.js (added 7 lines, deleted 2 lines).

--- components/ensemble.js
+++ components/ensemble.js
@@ -2,9 +2,14 @@
 var cavm = ee.FeatureCollection("projects/master-thesis-375622/assets/aga_circumpolar_geobotanical_2003"),
     glonaf = ee.FeatureCollection("projects/master-thesis-375622/assets/257_9_257_2_GloNAF_Shapefile"),
     bioVars = ee.Image("projects/master-thesis-375622/assets/WorldClim2-1Files/wc2-1_10m_bio_1"),
-    test = ee.ImageCollection("projects/master-thesis-375622/assets/worldClimDataCollection");
+    test = ee.ImageCollection("projects/master-thesis-375622/assets/worldClimDataCollection"),
+    testBio = ee.Image("WORLDCLIM/V1/BIO");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-var testBio = ee.Image("WORLDCLIM/V1/BIO")
+var bio1 = ee.Image("projects/master-thesis-375622/assets/WorldClim2-1Files/wc2-1_10m_bio_1");
+var bio = bio.addBands([
+  bio1,
+  "projects/master-thesis-375622/assets/WorldClim2-1Files/wc2-1_10m_bio_2"
+  ]);
 var bioClip = bioVars.clip(cavm);
 var gloClip = glonaf.filterBounds(cavm);
 var cavmGeo = cavm.geometry();
