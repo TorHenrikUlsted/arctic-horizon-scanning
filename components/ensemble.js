522c2881d84e4601ca73fcfcf0b56dc812abe11a
torhul <torhul@stud.ntnu.no>
2023-02-06 21:54:15 +0000

    Updated components/ensemble.js (added 2 lines, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -1,7 +1,8 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var cavm = ee.FeatureCollection("projects/master-thesis-375622/assets/aga_circumpolar_geobotanical_2003"),
     glonaf = ee.FeatureCollection("projects/master-thesis-375622/assets/257_9_257_2_GloNAF_Shapefile"),
-    bioVars = ee.Image("WORLDCLIM/V1/BIO");
+    bioVars = ee.Image("WORLDCLIM/V1/BIO"),
+    image = ee.Image("projects/master-thesis-375622/assets/raster_cavm_v1");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var bioClip = bioVars.clip(cavm);
 var gloClip = glonaf.filterBounds(cavm);
