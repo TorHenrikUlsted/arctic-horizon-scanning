9fc876e81ba5d58c28188376ce256fbd3aec65ed
torhul <torhul@stud.ntnu.no>
2023-02-06 22:23:14 +0000

    Updated components/ensemble.js (added 1 line, deleted 2 lines).

--- components/ensemble.js
+++ components/ensemble.js
@@ -5,7 +5,6 @@ var cavm = ee.FeatureCollection("projects/master-thesis-375622/assets/aga_circum
     tif = ee.Image("projects/master-thesis-375622/assets/raster_cavm_v1");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var cavmv2 = tif.geometry();
-print(cavmv2);
 
 //apply bounds
 var cavmv2PolygonBounds = cavmv2.bounds();
