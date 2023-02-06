14fc96869ab272b9cee2d606570dea3206d73982
torhul <torhul@stud.ntnu.no>
2023-02-06 15:01:42 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -6,7 +6,7 @@ var cavm = ee.FeatureCollection("projects/master-thesis-375622/assets/aga_circum
 var bioClip = bioVars.clip(cavm)
 Map.centerObject(cavm);
 
-var coll = ee.FeatureCollection(glonaf).map(cavm);
+var coll = ee.FeatureCollection(glonaf).map(bioClip);
 Map.addLayer(coll);
 
 //var arcticGlonaf = glonaf.clip(cavm);
