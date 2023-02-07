e57663c3c0bd249ed017e8377996e5bb7da96871
Håkon Harnes <hakon@harnes.dev>
2023-02-07 18:58:44 +0100

    fix: occurences -> occurrence

--- query/gbif_query.js
+++ query/gbif_query.js
@@ -5,7 +5,7 @@
 // Vertices of the polygon is specified as '(x1 y1, x2 y2, x3 y3, ...)'
 // Polygons must have anticlockwise ordering of points, or it will give unpredictable results
 const getOccurrencesInPolygon = async (vertices) => {
-  const response = await fetch(`https://api.gbif.org/v1/occurrences/search?geometry=POLYGON(${vertices})`)
+  const response = await fetch(`https://api.gbif.org/v1/occurrence/search?geometry=POLYGON(${vertices})`)
 
   if(response.status !== 200)
     return console.log('ERROR OCCURED:', response)
