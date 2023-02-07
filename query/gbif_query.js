9b15c01fcd9e92d2d51ac4d04933ac2e38dbfb7f
Håkon Harnes <hakon@harnes.dev>
2023-02-07 17:16:05 +0100

    fix: request to api.gbif.org/v1/occurrence/search?

--- query/gbif_query.js
+++ query/gbif_query.js
@@ -1,21 +1,16 @@
-var XMLHttpRequest = require('xhr2');
-var request = new XMLHttpRequest();
-var group = "/occurence";
-var searchKey = "/Pan troglodytes";
 
-request.open('GET', 'https://api.gbif.org/v1' + {group} + {searchKey}, true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach((data) => {
-      console.log(data)
-    })
-  } else {
-    console.log('error')
-  }
+// Rename this function to something that makes more sense
+// (I lack the domain knowledge)
+//
+// Add error handling if you want by checking data.status
+// Should be = 200 if the request was OK 
+const search = async (group, searchTerm) => {
+  const response = await fetch(`https://api.gbif.org/v1/${group}/search?${searchTerm}`)
+  const data = await response.json()
+
+  console.log(data)
 }
 
-request.send();
+search('occurrence', 'year=1800,1899')
 
