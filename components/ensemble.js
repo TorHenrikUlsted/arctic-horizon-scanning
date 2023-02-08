85311f75cc5613427052f6d768ce9f9dea2eb26f
torhul <torhul@stud.ntnu.no>
2023-02-08 15:33:45 +0000

    Updated components/ensemble.js (added 3 lines).

--- components/ensemble.js
+++ components/ensemble.js
@@ -8,7 +8,7 @@ var bioClip = bioVars.clip(cavm);
 var gloClip = glonaf.filterBounds(cavm);
 var test = cavm.geometry().geometries();
 
-print(bioClip.select('bio01');)
+print(bioClip.select('bio01'));
 
 
 var cavmProps = cavm.first().propertyNames().sort().slice(0, 10) //doesn't work properly for some reason
