da7fb88edcaf6251ef7570e17ca8acde78bd6740
torhul <torhul@stud.ntnu.no>
2023-02-08 15:06:12 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -6,7 +6,7 @@ var cavm = ee.FeatureCollection("projects/master-thesis-375622/assets/aga_circum
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var bioClip = bioVars.clip(cavm);
 var gloClip = glonaf.filterBounds(cavm);
-print(cavm.properties());
+print(cavm.props());
 var cavmImg = cavm.reduceToImage({
   properties: properties2, reducer: ee.Reducer.firstNonNull().forEach(properties2)
 });
