c9c684cd562b1dfc9c1899baf056f219e45b8532
torhul <torhul@stud.ntnu.no>
2023-02-08 15:10:38 +0000

    Updated components/ensemble.js (added 1 line, deleted 1 line).

--- components/ensemble.js
+++ components/ensemble.js
@@ -8,7 +8,7 @@ var bioClip = bioVars.clip(cavm);
 var gloClip = glonaf.filterBounds(cavm);
 
 var cavmProps = cavm.first().propertyNames().sort().slice(0, 11) //doesn't work properly for some reason
-print('properties2', cavmProps)
+print('CAVM properties', cavmProps)
 var cavmImg = cavm.reduceToImage({
   properties: cavmProps,
   reducer: ee.Reducer.firstNonNull().forEach(cavmProps)
