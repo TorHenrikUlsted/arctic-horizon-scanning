19c3b048624730badd98d5f6b7c1471646944a12
Tor Henrik Ulsted <tor_henrik_ulsted@hotmail.com>
2023-05-13 22:55:56 +0200

    AHS-33 feat: GloNAF wrangling
    - Removing unnecessary columns
    - Creating a list of only unique species names

---
+++ modeling/components/glonaf_wrangling.R
@@ -0,0 +1,13 @@
+# Here we wrangle with the GloNAF data to remove unecessary columns for the end list
+library(dplyr)
+## Read file
+glonaf_preformatted = read.csv("resources/glonaf.csv", header = T)
+
+## format the dataframe
+glonaf_formatted = select(glonaf_preformatted, c(standardized_name, family_tpl, region_id, status))
+
+## find all different statuses
+unique(glonaf_formatted$status)
+
+## get a list of all unique names
+glonaf_species = data.frame(standardized_name = unique(glonaf_formatted$standardized_name))
--- modeling/filter_data.R
+++ modeling/filter_data.R
@@ -1,1 +1,12 @@
 # here will the filtering take place by source("components/filename.r")
+# here i will filter all GBIF species from arctic and boreal against ABA and AMBIO
+# Also after that i will filter them against the glonaf list in the end, and all glonaf species that are not in the GBIF mix will be removed from the project.
+
+
+
+
+# conduct a synonym check for all datasets
+library(WorldFlora)
+# Conduct a synonym check using WFO
+glonaf_species_wfo = WFO.match(spec.data = glonaf_species, spec.name = "standardized_name", WFO.file = "resources/wfo_classification.csv", verbose = T, counter = 100)
+message("WFO completed the match")
